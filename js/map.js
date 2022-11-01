92e68272de1fd720651e147ad59e5ff1daa8b693
Garrett Nelson <91630285+garrettfnelson@users.noreply.github.com>
2022-11-01 17:57:14 -0600

    div formatting
    formatting

--- js/map.js
+++ js/map.js
@@ -9,10 +9,11 @@ window.onload = function () {
 }
 
 function renderMap () {
+
     var map = L.map('map').setView([37.5, -119], 6);
     
     var baseMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-	maxZoom: 16
+	  attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	  maxZoom: 16
   }).addTo(map);
 };
