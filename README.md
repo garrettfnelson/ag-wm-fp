fd9cdd971d8a642530fdf8c5d01d8766ab343f84
sayleekanderson <112521377+sayleekanderson@users.noreply.github.com>
2022-11-01 17:15:08 -0600

    map display

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 
 ### Final Proposal
 1. Group Members
-    1. Garrett
-    2. Trent
+    1. Garrett Nelson
+    2. Trent Sebatian
     3. Kaylee Sanderson
 2. Requirements Document
 
--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-//insert code here!
+"use strict";
+
+window.onload = function () {
+    renderMap();
+}
+
+function renderMap () {
+    var map = L.map('map').setView([37.5, -119], 6);
+    
+    var baseMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	maxZoom: 16
+  }).addTo(map);
+};
