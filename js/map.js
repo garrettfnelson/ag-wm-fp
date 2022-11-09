55f160ddd388cadf23d95191becd08030830bbe2
sayleekanderson <112521377+sayleekanderson@users.noreply.github.com>
2022-11-08 18:00:59 -0700

    map points

--- js/map.js
+++ js/map.js
@@ -1,14 +1,44 @@
 "use strict";
 
 window.onload = function () {
-    renderMap();
+  renderMap();
+  addLayers();
 }
 
-function renderMap () {
-    var map = L.map('map').setView([37.5, -119], 6);
-    
-    var baseMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-	maxZoom: 16
+function renderMap() {
+  var map = L.map('map').setView([37.5, -119], 6);
+
+  var baseMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+    maxZoom: 16
   }).addTo(map);
+
+}
+
+
+function pointToCircle(feature, latlng) {
+  let fillColorVar = "";
+  let radiusColor = "";
+
+  let geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: fillColorVar,
+    color: radiusColor,
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+  };
+
+  let circleMarker = L.circleMarker(latlng, geojsonMarkerOptions);
+  return circleMarker;
+}
+
+function addLayers() {
+  let countiesLayer = L.geoJSON(counties, {
+    pointToLayer: pointToCircle
+  });
+
+  map.addLayer(countiesLayer);
 };
+
+
