526195e50f6723208bcd6709321d71bc8560d65e
Garrett Nelson <91630285+garrettfnelson@users.noreply.github.com>
2022-11-15 18:42:34 -0700

    heatmap updated
    heatmap

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,115 +1,139 @@
-// set the dimensions and margins of the graph
-const margin = { top: 80, right: 25, bottom: 30, left: 40 },
-    width = 450 - margin.left - margin.right,
-    height = 450 - margin.top - margin.bottom;
+// Margin Convention
+let margins = { top: 20, right: 25, bottom: 30, left: 40 }
+let outerWidth = 450
+let outerHeight = 450
+let innerWidth = 450 - margins.left - margins.right
+let innerHeight = 450 - margins.top - margins.bottom
 
 // append the svg object to the body of the page
-const svg = d3.select("chart")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+let svg = d3
+  .select('div#graph svg')
+  .attr('width', outerWidth)
+  .attr('height', outerHeight)
+  .append('g')
+  .attr('id', 'plot-area')
+  .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')')
 
-//Read the data
-d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv").then(function (data) {
+// read the data, then draw the graph
+d3.csv(
+  'https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv'
+).then(draw)
 
-    // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
-    const myGroups = Array.from(new Set(data.map(d => d.group)))
-    const myVars = Array.from(new Set(data.map(d => d.variable)))
+function draw(data) {
+  console.log(data)
+  // labels of row and columns -> unique identifier of the column called 'group' and 'variable'
+  var groups = d3.map(data, d => d.group).keys()
+  var vars = d3.map(data, d => d.variable).keys()
 
-    // Build X scales and axis:
-    const x = d3.scaleBand()
-        .range([0, width])
-        .domain(myGroups)
-        .padding(0.05);
-    svg.append("g")
-        .style("font-size", 15)
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x).tickSize(0))
-        .select(".domain").remove()
+  // build x scale and axis:
+  var xScale = d3
+    .scaleBand()
+    .domain(groups)
+    .range([0, innerWidth])
+    .padding(0.1)
 
-    // Build Y scales and axis:
-    const y = d3.scaleBand()
-        .range([height, 0])
-        .domain(myVars)
-        .padding(0.05);
-    svg.append("g")
-        .style("font-size", 15)
-        .call(d3.axisLeft(y).tickSize(0))
-        .select(".domain").remove()
+  svg
+    .append('g')
+    .style('font-size', 15)
+    .attr('transform', 'translate(0,' + innerHeight + ')')
+    .call(d3.axisBottom(xScale).tickSize(0))
+    .select('.domain') // remove horizontal line from axis
+    .remove()
 
-    // Build color scale
-    const myColor = d3.scaleSequential()
-        .interpolator(d3.interpolateInferno)
-        .domain([1, 100])
+  // Build y scale and axis:
+  var yScale = d3
+    .scaleBand()
+    .domain(vars)
+    .range([innerHeight, 0])
+    .padding(0.05)
 
-    // create a tooltip
-    const tooltip = d3.select("#chart")
-        .append("div")
-        .style("opacity", 0)
-        .attr("class", "tooltip")
-        .style("background-color", "white")
-        .style("border", "solid")
-        .style("border-width", "2px")
-        .style("border-radius", "5px")
-        .style("padding", "5px")
+  svg
+    .append('g')
+    .style('font-size', 15)
+    .call(d3.axisLeft(yScale).tickSize(0))
+    .select('.domain') // remove vertical line from axis
+    .remove()
 
-    // Three function that change the tooltip when user hover / move / leave a cell
-    const mouseover = function (event, d) {
-        tooltip
-            .style("opacity", 1)
-        d3.select(this)
-            .style("stroke", "black")
-            .style("opacity", 1)
-    }
-    const mousemove = function (event, d) {
-        tooltip
-            .html("The exact value of<br>this cell is: " + d.value)
-            .style("left", (event.x) / 2 + "px")
-            .style("top", (event.y) / 2 + "px")
-    }
-    const mouseleave = function (event, d) {
-        tooltip
-            .style("opacity", 0)
-        d3.select(this)
-            .style("stroke", "none")
-            .style("opacity", 0.8)
-    }
+  // build color scale
+  let colorScale = d3
+    .scaleSequential()
+    .interpolator(d3.interpolateInferno)
+    .domain([1, 100])
 
-    // add the squares
-    svg.selectAll()
-        .data(data, function (d) { return d.group + ':' + d.variable; })
-        .join("rect")
-        .attr("x", function (d) { return x(d.group) })
-        .attr("y", function (d) { return y(d.variable) })
-        .attr("rx", 4)
-        .attr("ry", 4)
-        .attr("width", x.bandwidth())
-        .attr("height", y.bandwidth())
-        .style("fill", function (d) { return myColor(d.value) })
-        .style("stroke-width", 4)
-        .style("stroke", "none")
-        .style("opacity", 0.8)
-        .on("mouseover", mouseover)
-        .on("mousemove", mousemove)
-        .on("mouseleave", mouseleave)
-})
+  // add the squares
+  svg
+    .selectAll()
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('x', d => xScale(d.group))
+    .attr('y', d => yScale(d.variable))
+    .attr('rx', 4)
+    .attr('ry', 4)
+    .attr('width', xScale.bandwidth())
+    .attr('height', yScale.bandwidth())
+    .style('fill', d => colorScale(d.value))
+    .style('stroke', 'black')
+    .style('stroke-width', 4)
+    .style('stroke-opacity', 0)
+    .style('fill-opacity', 0.7)
+    .on('mouseover', darken_square)
+    .on('mousemove', show_info)
+    .on('mouseleave', function(d) {
+      hide_info()
+      lighten_square(d, this) // need to explicitly pass this
+    })
+    .on('click', change_border_color)
 
-// Add title to graph
-svg.append("text")
-    .attr("x", 0)
-    .attr("y", -50)
-    .attr("text-anchor", "left")
-    .style("font-size", "22px")
-    .text("A d3.js heatmap");
+  // three functions that change the tooltip when user hovers / moves in / leaves a cell
+  // because we use the function key word to declare them, we can put them down
+  // here and they will be hoisted.
 
-// Add subtitle to graph
-svg.append("text")
-    .attr("x", 0)
-    .attr("y", -20)
-    .attr("text-anchor", "left")
-    .style("font-size", "14px")
-    .style("fill", "grey")
-    .style("max-width", 400)
-    .text("A short description of the take-away message of this chart.");
+  function darken_square(d) {
+    d3.select(this)
+      .style('fill-opacity', 1)
+      .style('stroke-opacity', 1)
+      // shrink a bit to make room for stroke, now visible
+      .attr('x', d => xScale(d.group) + 2)
+      .attr('y', d => yScale(d.variable) + 2)
+      .attr('width', xScale.bandwidth() - 4)
+      .attr('height', yScale.bandwidth() - 4)
+  }
+
+  function show_info(d) {
+    let mouseLoc = d3.mouse(this)
+    let info =
+      'The exact value of this cell is: ' +
+      d.value +
+      '. ' +
+      '<br />Mouse location is: (' +
+      mouseLoc[0] +
+      ', ' +
+      mouseLoc[1] +
+      ').'
+
+    // .html instead of .text() allows us to supply html markup here
+    d3.selectAll('.tooltip, .info')
+      .html(info)
+      .style('visibility', 'visible')
+      // left and top only affect .tooltip b/c position = absolute -- see css
+      .style('left', mouseLoc[0] + margins.left + xScale.bandwidth() + 'px')
+      .style('top', mouseLoc[1] - yScale.bandwidth() + 'px')
+  }
+
+  function hide_info() {
+    d3.selectAll('.tooltip, .info').style('visibility', 'hidden')
+  }
+
+  function lighten_square(d, ref) {
+    d3.select(ref)
+      .style('stroke-opacity', 0.5)
+      .style('fill-opacity', 0.7)
+  }
+
+  function change_border_color(d) {
+    let target = d3.select(this)
+    let color = target.style('stroke') // read the current stroke value
+    target.style('stroke', color == 'red' ? 'blue' : 'red')
+  }
+}
